cbe22211b089fda7ade17835d66f0851d632ff8d
gujial <gu18647403665@outlook.com>
2025-05-18 16:42:25 +0800

    feat: 完成首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,24 @@
 // 首页逻辑
 Page({
   data: {
-    welcomeText: '欢迎使用背单词小程序！'
+    welcomeText: '欢迎使用!',
+    intro: '记得经常复习，提高你的英语词汇量吧'
   },
   // 跳转到学习页面
-  goToLearn: function() {
+  goToLearn: function () {
     wx.navigateTo({
       url: '/pages/learn/learn'
     })
   },
-  // 跳转到排行榜页面
-  goToRanking: function() {
-    wx.navigateTo({
-      url: '/pages/ranking/ranking'
+  async onShow() {
+    var page = this;
+    wx.request({
+      'url': 'https://v1.hitokoto.cn/?c=d&c=i',
+      success(response) {
+        page.setData({
+          intro: response.data.hitokoto+`(${response.data.from_who}《${response.data.from}》)`
+        });
+      }
     })
-  },
-  // 跳转到我的页面
-  goToMe: function() {
-      wx.navigateTo({
-        url: '/pages/me/me',
-      })
   }
 })
